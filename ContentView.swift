50ee929238cbeac914f9d1fffb98ba58020d13ab
ddd <dedeten2@naver.com>
2020-03-18 06:03:25 +0900

    init

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+d//
+//  ContentView.swift
+//  nws
+//
+//  Created by macproo on 2020/03/18.
+//  Copyright © 2020 ddd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
